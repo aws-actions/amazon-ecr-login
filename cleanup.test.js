b4f084e928b56f43c9afa5773b761bbfcd7e83e2
Arjun Raman <arjraman@amazon.com>
2022-06-13 14:53:54 -0700

    feat: added ECR Public Registry support

--- cleanup.test.js
+++ cleanup.test.js
@@ -5,13 +5,22 @@ const exec = require('@actions/exec');
 jest.mock('@actions/core');
 jest.mock('@actions/exec');
 
+function mockGetState(requestResponse) {
+  return function (name, options) { // eslint-disable-line no-unused-vars
+    return requestResponse[name]
+  }
+}
+
+const ECR_STATES = {
+  'registries': '123456789012.dkr.ecr.aws-region-1.amazonaws.com,111111111111.dkr.ecr.aws-region-1.amazonaws.com'
+};
+
 describe('Logout from ECR', () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
 
-    core.getState.mockReturnValue(
-      '123456789012.dkr.ecr.aws-region-1.amazonaws.com,111111111111.dkr.ecr.aws-region-1.amazonaws.com');
+    core.getState = jest.fn().mockImplementation(mockGetState(ECR_STATES));
     exec.exec.mockReturnValue(0);
   });
 
@@ -19,8 +28,6 @@ describe('Logout from ECR', () => {
     await cleanup();
 
     expect(core.getState).toHaveBeenCalledWith('registries');
-
-    expect(exec.exec).toHaveBeenCalledTimes(2);
     expect(exec.exec).toHaveBeenNthCalledWith(1,
       'docker',
       ['logout', '123456789012.dkr.ecr.aws-region-1.amazonaws.com'],
@@ -29,17 +36,36 @@ describe('Logout from ECR', () => {
       'docker',
       ['logout', '111111111111.dkr.ecr.aws-region-1.amazonaws.com'],
       expect.anything());
-
+    expect(exec.exec).toHaveBeenCalledTimes(2);
     expect(core.setFailed).toHaveBeenCalledTimes(0);
   });
 
-  test('handles zero registries', async () => {
-    core.getState.mockReturnValue('');
+  test('logs out docker client for public registry in action state', async () => {
+    const mockStates = {
+      'registries': 'public.ecr.aws'
+    };
+    core.getState = jest.fn().mockImplementation(mockGetState(mockStates));
 
     await cleanup();
 
     expect(core.getState).toHaveBeenCalledWith('registries');
+    expect(exec.exec).toHaveBeenNthCalledWith(1,
+      'docker',
+      ['logout', 'public.ecr.aws'],
+      expect.anything());
+    expect(exec.exec).toHaveBeenCalledTimes(1);
+    expect(core.setFailed).toHaveBeenCalledTimes(0);
+  });
 
+  test('handles zero registries', async () => {
+    const mockStates = {
+      'registries' : ''
+    };
+    core.getState = jest.fn().mockImplementation(mockGetState(mockStates));
+
+    await cleanup();
+
+    expect(core.getState).toHaveBeenCalledWith('registries');
     expect(exec.exec).toHaveBeenCalledTimes(0);
     expect(core.setFailed).toHaveBeenCalledTimes(0);
   });
@@ -49,34 +75,31 @@ describe('Logout from ECR', () => {
 
     await cleanup();
 
-    expect(core.setFailed).toBeCalled();
+    expect(core.setFailed).toHaveBeenCalled();
   });
 
   test('continues to attempt logouts after a failed logout', async () => {
-    core.getState.mockReturnValue(
-      '123456789012.dkr.ecr.aws-region-1.amazonaws.com,111111111111.dkr.ecr.aws-region-1.amazonaws.com,222222222222.dkr.ecr.aws-region-1.amazonaws.com');
-    exec.exec
-      .mockImplementationOnce((commandLine, args, options) => {
-        options.listeners.stdout('stdout of ');
-        options.listeners.stdout('registry 1');
-        options.listeners.stderr('stderr of ');
-        options.listeners.stderr('registry 1');
-        return(1);
-      })
-      .mockImplementationOnce((commandLine, args, options) => {
-        options.listeners.stdout('stdout of ');
-        options.listeners.stdout('registry 2');
-        options.listeners.stderr('stderr of ');
-        options.listeners.stderr('registry 2');
-        return(1);
-      })
-      .mockReturnValueOnce(0);
+    const mockStates = {
+      'registries' : '123456789012.dkr.ecr.aws-region-1.amazonaws.com,111111111111.dkr.ecr.aws-region-1.amazonaws.com,222222222222.dkr.ecr.aws-region-1.amazonaws.com'
+    };
+    core.getState = jest.fn().mockImplementation(mockGetState(mockStates));
+    exec.exec.mockImplementationOnce((commandLine, args, options) => {
+      options.listeners.stdout('stdout of ');
+      options.listeners.stdout('registry 1');
+      options.listeners.stderr('stderr of ');
+      options.listeners.stderr('registry 1');
+      return(1);
+    }).mockImplementationOnce((commandLine, args, options) => {
+      options.listeners.stdout('stdout of ');
+      options.listeners.stdout('registry 2');
+      options.listeners.stderr('stderr of ');
+      options.listeners.stderr('registry 2');
+      return(1);
+    }).mockReturnValueOnce(0);
 
     await cleanup();
 
     expect(core.getState).toHaveBeenCalledWith('registries');
-
-    expect(exec.exec).toHaveBeenCalledTimes(3);
     expect(exec.exec).toHaveBeenNthCalledWith(1,
       'docker',
       ['logout', '123456789012.dkr.ecr.aws-region-1.amazonaws.com'],
@@ -89,12 +112,11 @@ describe('Logout from ECR', () => {
       'docker',
       ['logout', '222222222222.dkr.ecr.aws-region-1.amazonaws.com'],
       expect.anything());
-
-    expect(core.error).toHaveBeenCalledTimes(2);
-    expect(core.error).toHaveBeenNthCalledWith(1, 'Could not logout registry 123456789012.dkr.ecr.aws-region-1.amazonaws.com: stderr of registry 1');
-    expect(core.error).toHaveBeenNthCalledWith(2, 'Could not logout registry 111111111111.dkr.ecr.aws-region-1.amazonaws.com: stderr of registry 2');
-
-    expect(core.setFailed).toHaveBeenCalledTimes(1);
+    expect(core.error).toHaveBeenNthCalledWith(1, 'Could not logout of registry 123456789012.dkr.ecr.aws-region-1.amazonaws.com: stderr of registry 1');
+    expect(core.error).toHaveBeenNthCalledWith(2, 'Could not logout of registry 111111111111.dkr.ecr.aws-region-1.amazonaws.com: stderr of registry 2');
     expect(core.setFailed).toHaveBeenCalledWith('Failed to logout: 123456789012.dkr.ecr.aws-region-1.amazonaws.com,111111111111.dkr.ecr.aws-region-1.amazonaws.com');
+    expect(exec.exec).toHaveBeenCalledTimes(3);
+    expect(core.error).toHaveBeenCalledTimes(2);
+    expect(core.setFailed).toHaveBeenCalledTimes(1);
   });
 });
